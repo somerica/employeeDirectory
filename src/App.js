08a2b5deb5386a0d8086910a02a6400834e70969
Somer Jackson <somer.jackson@gmail.com>
2021-02-07 19:16:25 -0500

    add sort filter functions

--- src/App.js
+++ src/App.js
@@ -18,11 +18,42 @@ function App() {
     })
   }, [])
 
-  
+  const sortByLastName = (employee) => {
+    let sortedEmployees = [...employee];
+
+    sortedEmployees.sort((a, b) => {
+      if (a.name.last < b.name.last) {
+        return -1;
+      }
+      if (a.name.last > b.name.last) {
+        return 1;
+      }
+      return 0;
+    })
+
+    // Sort the employees
+    setEmployees(sortedEmployees);
+  }
+
+  const filterFemales = (employee) => {
+    let femaleEmployees = [...employee]
+    
+    const filteredFemales = femaleEmployees.filter(employ => employ.gender === 'female');
+
+    setEmployees(filteredFemales);
+  }
 
   return (
     <div className="App">
       <h1>Employee Directory</h1>
+      <div>
+      <button onClick={() => {
+        sortByLastName(employees)    
+      }}>Sort By Last Name</button>
+      <button onClick={() => {
+        filterFemales(employees);
+      }}>Filter Female</button>
+      </div>
       <Employee employees={employees}/>
     </div>
   );
